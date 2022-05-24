faed4ac21fed32225ae92a0c2e33b7f90918a0db
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-05-24 17:50:39 -0300

    feat: add Mage, Necromancer, Warrior and Ranger Archetypes

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _createdInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._createdInstances += 1;
+    return this._createdInstances;
+  }
+}
